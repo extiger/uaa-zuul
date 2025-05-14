b407faeec2714d00be538f05a816c54fa6a1435a
chao <6@qq.com>
2025-05-14 22:02:55 +0800

    Revert "b1提交2"
    This reverts commit 23da517b4d2301529eeab6366bef314cfb6156e5.

--- auth-server/src/main/java/com/hfcsbc/controller/UserController.java
+++ auth-server/src/main/java/com/hfcsbc/controller/UserController.java
@@ -12,7 +12,7 @@ import java.security.Principal;
 @RestController
 public class UserController {
 
-    @GetMapping("/user1111")
+    @GetMapping("/user111")
     public Principal user(Principal user){
         return user;
     }

cfede0a688168057e2813d07c1ba1d8aedb32577
chao <6@qq.com>
2025-05-14 22:12:52 +0800

    b1提交3

--- auth-server/src/main/java/com/hfcsbc/controller/UserController.java
+++ auth-server/src/main/java/com/hfcsbc/controller/UserController.java
@@ -12,7 +12,7 @@ import java.security.Principal;
 @RestController
 public class UserController {
 
-    @GetMapping("/user111")
+    @GetMapping("/user1113")
     public Principal user(Principal user){
         return user;
     }
